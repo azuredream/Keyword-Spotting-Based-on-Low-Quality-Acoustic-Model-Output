5fefc79bdf629c7e93438b099aa0b7ade0a0228a
zixuan zhao <1919037664@qq.com>
2019-04-05 14:44:31 +0800

    Update spotting.py
    add demo
    Restructure to module

--- spotting.py
+++ spotting.py
@@ -2,41 +2,47 @@ import os
 from xpinyin import Pinyin
 from fuzzywuzzy import fuzz
 
-simithreshold = 78 #%
-#您好请稍等；再见；请交费
-Targetlist = ["ninhaoqingshaodeng","zaijian","qingjiaofei"]
-Targetdict = {"ninhaoqingshaodeng":"您好请稍等;","zaijian":"再见;""qingjiaofei":"请交费;"}
+class keywordSpoter(object):
+	"""docstring for keywordSpoter"""
+	def __init__(self, keywordlist,simithreshold = 78):
+		super(keywordSpoter, self).__init__()
+		self.keywordlist = keywordlist
+		self.simithreshold = simithreshold
+		self.Targetlist = []
+		self.Targetdict = {}
+		pin = Pinyin()
+		for i in keywordlist:
+			ipinyin = pin.get_pinyin(i,"")
+			self.Targetlist.append(ipinyin)
+			self.Targetdict[ipinyin] = i+";"
+
+	def simi(self,a,b):
+		return fuzz.ratio(a,b)
+
+	def getWord(self,input):
+		pin = Pinyin()
+		a = pin.get_pinyin(input,"")
+		result = ""
+		for i in self.Targetlist:#对每个关键词
+			windowwidth = len(i)#以目标词长度为窗口，遍历输入串
+			j = 0
+			preratio = 0#当相似度从阈值以上变化到阈值以下，记为一次命中
+			ratio = 0
+			while(j+windowwidth<=len(a)):
+				preratio = ratio
+				ratio = self.simi(a[j:j+windowwidth],i)
+				#print(i+":"+a[j:j+windowwidth]+":"+str(ratio))
+				if preratio>self.simithreshold and ratio<self.simithreshold:	
+					#print(a[j-1:j+windowwidth-1],"simi",preratio)
+					result+=self.Targetdict[i]#当相似度从阈值以上变化到阈值以下，记为一次命中，将它计入结果
+				j+=1
+			if ratio>self.simithreshold:#如果目标词在末尾，相似度不会回落，直接计入
+				result+=self.Targetdict[i]
+		return result
 
 
-
-
-def simi(a,b):
-	return fuzz.ratio(a,b)
-
-def getWord(input):
-	pin = Pinyin()
-	a = pin.get_pinyin(input,"")
-
-	result = ""
-	#cursimi = 0
-	for i in Targetlist:#对每个关键词
-		windowwidth = len(i)#以目标词长度为窗口，遍历输入串
-		j = 0
-		preratio = 0#当相似度从阈值以上变化到阈值以下，记为一次命中
-		ratio = 0
-		while(j+windowwidth<=len(a)):
-			preratio = ratio
-			ratio = simi(a[j:j+windowwidth],i)
-			#print(i+":"+a[j:j+windowwidth]+":"+str(ratio))
-			if preratio>simithreshold and ratio<simithreshold:	
-				#print(a[j-1:j+windowwidth-1],"simi",preratio)
-				result+=Targetdict[i]#当相似度从阈值以上变化到阈值以下，记为一次命中，将它计入结果
-			j+=1
-		if ratio>simithreshold:#如果目标词在末尾，相似度不会回落，直接计入
-			result+=Targetdict[i]
-		#ratio = simi(a,i)
-		#if ratio>simithreshold and ratio>cursimi:
-		#	cursimi = simi(a,i)
-		#	result = i
-	#print("simi:",cursimi)
-	return result
+if __name__ == '__main__':
+	while(True):
+		ss = keywordSpoter(["你好","再见","没错"])
+		inputstr = input("Intput your str:")
+		print(ss.getWord(inputstr))
